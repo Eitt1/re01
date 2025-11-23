d5504bd859f4147bb054373f4c86168c82b299d0
Eitt1 <95868893+Eitt1@users.noreply.github.com>
2025-11-24 00:29:19 +0800

    Update server.js

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ app.use(passport.session());
 
 
 const facebookAuth = {
-    'clientID': '902102522477336', 
-    'clientSecret': '267a3fdc14c82f38f5b45ed333cc95ef', 
+    'clientID': '3736274196677931', 
+    'clientSecret': 'ce8840d46db118a4655065ceffc22fbd', 
     'callbackURL': 'https://comp3810sef-group63.onrender.com/auth/facebook/callback'
 };
 
@@ -272,6 +272,7 @@ app.get(/(.*)/, (req, res) => {
 
 const port = process.env.PORT || 8099;
 app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
+
 
 
 
